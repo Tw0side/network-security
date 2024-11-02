fd8981ddb539565eb7b393f3b80570fb5b0c5473
Santo cyriac <132189839+Tw0side@users.noreply.github.com>
2024-11-02 09:58:32 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,31 +1,67 @@
-#include<sys/socket.h>
-#include <netdb.h>
-#include <string.h> 
-#include <stdlib.h>
-#include netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
-#include <string.h> 
+#include <stdlib.h>
+#include <string.h>
 #include <time.h>
-void main()
-{
-  int sd ,addrlen ,num =1;
-time_t start time, rt, current time; struct
-sockaddr in servaddr, cliaddr; 
-sd=socket(AF_INET, SOCK_DGRAM,0);
-servaddr.sin family=AF_INET;
-servaddr.sin addr.s_addr=htonl(INADDR_ANY);
-servaddr.sin _port-=htons(43454); 
-cliaddr.sin family=AF_INET;
-cliaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-cliaddr.sin_port=htons(43455);
-bind(sd,(struct sockadddr *)&cliaddr,sizeof(cliaddr));
-printf("Client is running......................"\n);
-start_time=time(NULL);
-sendto(sd, &num, sizeof(num), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
-addrlen=sizeof(cliaddr);
-recvfrom(sd,&current_time,sizeof(current_time),0,(struct sockaddr*) & cliaddr *) &cliaddr ,&addrlen);
-rtt = time(NULL)-start_time;
-current_time +=rtt/2;
-printf("Servers Time :%s\n,",ctime(&current_time));
+#include <unistd.h> // for close()
+
+int main() {
+    int sd, addrlen, num = 1;
+    time_t start_time, rt, current_time; 
+    struct sockaddr_in servaddr, cliaddr; 
+
+    // Create socket
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up server address
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(43454); 
+
+    // Set up client address
+    bzero(&cliaddr, sizeof(cliaddr));
+    cliaddr.sin_family = AF_INET;
+    cliaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    cliaddr.sin_port = htons(43455);
+
+    // Bind the socket
+    if (bind(sd, (struct sockaddr *)&cliaddr, sizeof(cliaddr)) < 0) {
+        perror("Bind failed");
+        close(sd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Client is running......................\n");
+
+    // Get the start time
+    start_time = time(NULL);
+
+    // Send a request to the server
+    sendto(sd, &num, sizeof(num), 0, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    addrlen = sizeof(cliaddr);
+
+    // Receive the current time from the server
+    if (recvfrom(sd, &current_time, sizeof(current_time), 0, (struct sockaddr *) &cliaddr, &addrlen) < 0) {
+        perror("recvfrom failed");
+        close(sd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Calculate round-trip time and adjust current time
+    rt = time(NULL) - start_time;
+    current_time += rt / 2;
+
+    printf("Server's Time: %s\n", ctime(&current_time));
+
+    // Close the socket
+    close(sd);
+    return 0;
 }
